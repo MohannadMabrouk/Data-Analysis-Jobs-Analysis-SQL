beb78af356bb05d3644ddb53e517138213d66e74
Mohannad <mohannadmabrouk223@gmail.com>
2024-05-25 16:36:23 +0300

    adding the project file

---
+++ project_sql/1_top_paying_job.sql
@@ -0,0 +1,6 @@
+/*
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely
+- Focuses on jeb postings with specified salaries (remove nulls).
+- Why? Highlight the top-paying oppottunities for Data Analyst, offering insights into emplo
+*/
