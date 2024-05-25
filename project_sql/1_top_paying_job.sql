ced4d36c5cd63c29cf8afadf2d2c0e50eb7e5cee
Mohannad <mohannadmabrouk223@gmail.com>
2024-05-25 17:53:58 +0300

    Solved 1st Question: What are the top-paying data analyst jobs?

--- project_sql/1_top_paying_job.sql
+++ project_sql/1_top_paying_job.sql
@@ -4,3 +4,24 @@ Question: What are the top-paying data analyst jobs?
 - Focuses on jeb postings with specified salaries (remove nulls).
 - Why? Highlight the top-paying oppottunities for Data Analyst, offering insights into emplo
 */
+
+SELECT 
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    company_dim.name
+FROM   
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY   
+    salary_year_avg DESC
+LIMIT 10;    
+
+
